bc2d749cd0ba5d4311bc7c102d3d966c06423bbf
Seth NeJame <sethnejame@gmail.com>
2019-10-27 10:07:55 +0100

    Iterating over post data @ body

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -3,18 +3,27 @@ import React, { Component } from "react";
 class Posts extends Component {
   state = {
     posts: []
-  }
+  };
 
   componentWillMount() {
     fetch("https://jsonplaceholder.typicode.com/posts")
       .then(response => response.json())
-      .then(data => console.log(data));
+      .then(data => this.setState({ posts: data }));
+    console.log(this.state.posts);
   }
 
   render() {
+    const postItems = this.state.posts.map(post => (
+      <div key={post.id}>
+        <h4>{post.title}</h4>
+        <p>{post.body}</p>
+        <hr></hr>
+      </div>
+    ));
     return (
       <div>
         <h1>Posts</h1>
+        {postItems}
       </div>
     );
   }
