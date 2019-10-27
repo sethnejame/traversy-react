9d2a2908a32ae03c6030ff973c4654e2bfed1af4
Seth NeJame <sethnejame@gmail.com>
2019-10-27 15:58:27 +0100

    Adding FETCH_POSTS action to postReducer

--- src/redux/reducers/postReducer.js
+++ src/redux/reducers/postReducer.js
@@ -8,7 +8,7 @@ const initialState = {
 export default function(state = initialState, action) {
   switch (action.type) {
     case FETCH_POSTS:
-      //TODO
+      return { ...state, items: action.payload };
       break;
     case NEW_POSTS:
       //TODO
