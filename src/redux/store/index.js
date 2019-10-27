9813402ebef4d5a5f3261fa75326f5e35afe154a
Seth NeJame <sethnejame@gmail.com>
2019-10-27 13:24:39 +0100

    Adding middleware (thunk) to store, store index now exports instatiated store

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,6 +1,15 @@
 import { createStore, applyMiddleware } from "redux";
 import rootReducer from "../reducers";
+import thunk from "redux-thunk";
 
-export default function configureStore(initialState) {
-  return createStore(rootReducer, initialState, applyMiddleware());
-}
+const initialState = {};
+
+const middleware = [thunk];
+
+const store = createStore(
+  rootReducer,
+  initialState,
+  applyMiddleware(...middleware)
+);
+
+export default store;
