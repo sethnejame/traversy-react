ea30fd7cf78f8598ac943e29952821f1ccc3d79b
Seth NeJame <sethnejame@gmail.com>
2019-10-27 15:52:54 +0100

    Adding fetchPosts action

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,15 +1,6 @@
 import React, { Component } from "react";
 
 class Posts extends Component {
-  state = {
-    posts: []
-  };
-
-  componentWillMount() {
-    fetch("https://jsonplaceholder.typicode.com/posts")
-      .then(response => response.json())
-      .then(data => this.setState({ posts: data }));
-  }
 
   render() {
     const postItems = this.state.posts.map(post => (
