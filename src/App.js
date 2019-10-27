8ee3ac34e77447ab8e9283c2bad286ee677eace2
Seth NeJame <sethnejame@gmail.com>
2019-10-27 11:00:53 +0100

    Adding store to App

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import React from "react";
 import Posts from "./components/Posts";
 import PostForm from "./components/PostForm";
 import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+
+const store = createStore(() => [], {}, applyMiddleware());
 
 const App = () => (
-  <Provider>
+  <Provider store={store}>
     <div className="container-fluid">
       <PostForm />
       <hr />
