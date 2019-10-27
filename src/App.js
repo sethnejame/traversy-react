cdbde56f9d798080a537ae51dca9f293a4d36f6d
Seth NeJame <sethnejame@gmail.com>
2019-10-27 10:55:34 +0100

    Wrapping App component in Provider

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
 import Posts from "./components/Posts";
 import PostForm from "./components/PostForm";
+import { Provider } from "react-redux";
 
 const App = () => (
-  <div className="container-fluid">
-    <PostForm />
-    <hr />
-    <Posts />
-  </div>
+  <Provider>
+    <div className="container-fluid">
+      <PostForm />
+      <hr />
+      <Posts />
+    </div>
+  </Provider>
 );
 
 export default App;
