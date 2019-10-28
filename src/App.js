4690c9bd0b0cf33e418d23509d21eee8ba85d69a
Seth NeJame <sethnejame@gmail.com>
2019-10-28 07:04:23 +0100

    Fixing store import from store index to App

--- src/App.js
+++ src/App.js
@@ -2,9 +2,7 @@ import React from "react";
 import Posts from "./components/Posts";
 import PostForm from "./components/PostForm";
 import { Provider } from "react-redux";
-import { configureStore } from "./redux/store";
-
-const store = configureStore();
+import store from "./redux/store";
 
 const App = () => (
   <div className="container-fluid">
--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,6 +1,8 @@
 import { createStore, applyMiddleware } from "redux";
-import rootReducer from "../reducers/";
+import rootReducer from "../reducers";
 
-export default function configureStore(initialState) {
-  createStore(rootReducer, initialState, applyMiddleware());
-}
+const initialState = {};
+
+const store = createStore(rootReducer, initialState, applyMiddleware());
+
+export default store;
