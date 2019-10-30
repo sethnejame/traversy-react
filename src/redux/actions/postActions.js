551e8b8e3331ce55317ed3cfe87cc94ff63044dc
Seth NeJame <sethnejame@gmail.com>
2019-10-30 07:01:45 +0100

    Adding fetchPosts to postActions

---
+++ src/redux/actions/postActions.js
@@ -0,0 +1,9 @@
+import { FETCH_POSTS, NEW_POST } from "./types";
+
+export function fetchPosts() {
+  return function(dispatch) {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then(response => response.json())
+      .then(posts => dispatch({ type: FETCH_POSTS, payload: posts }));
+  };
+}
