38ef17d67264aedfe2e06b5bf2dbd6f0230833f6
Seth NeJame <sethnejame@gmail.com>
2019-10-30 06:53:20 +0100

    Adding basic postReducer

---
+++ src/redux/reducers/postReducer.js
@@ -0,0 +1,13 @@
+import { FETCH_POSTS, NEW_POST } from "../actions/types";
+
+const initialState = {
+  items: [], // the reducer needs an init state - this is for the array of posts
+  item: {} // this is for the post object (contains title, body)
+};
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
