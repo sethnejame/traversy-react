fd9d1a8e2f9ce1d05a8ed4c35f3bcc7c207f813a
Seth NeJame <sethnejame@gmail.com>
2019-10-30 06:15:01 +0100

    Adding redux thunk

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,8 +1,15 @@
 import { createStore, applyMiddleware } from "redux";
 import rootReducer from "../reducers";
+import thunk from "redux-thunk";
 
 const initialState = {};
 
-const store = createStore(rootReducer, initialState, applyMiddleware());
+const middleware = [thunk];
+
+const store = createStore(
+  rootReducer,
+  initialState,
+  applyMiddleware(...middleware)
+);
 
 export default store;
