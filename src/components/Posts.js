52f55b4432f0be9d65967dd20e65c96920f524a1
Seth NeJame <sethnejame@gmail.com>
2019-10-30 07:21:25 +0100

    Adding proptypes for props validation @ Posts

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { fetchPosts } from "../actions/types";
 import { connect } from "react-redux";
+import PropTypes from "prop-types";
 
 class Posts extends Component {
   componentWillMount() {
@@ -24,9 +25,14 @@ class Posts extends Component {
   }
 }
 
+Posts.propTypes = {
+  fetchPosts: PropTypes.func.isRequired,
+  posts: PropTypes.array.isRequired
+};
+
 const mapStateToProps = state => ({
   posts: state.posts.items
-})
+});
 
 export default connect(
   null,
