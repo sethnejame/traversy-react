18df5eda531bcdcb06d605ceec4d88761ea99cdd
Seth NeJame <sethnejame@gmail.com>
2019-11-04 07:11:24 +0100

    Adding form inputs to PostForm

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import Posts from "./components/Posts";
 import PostForm from "./components/PostForm";
 
 const App = () => (
-  <>
+  <div className="container-fluid">
     <Posts />
     <hr />
     <PostForm />
-  </>
+  </div>
 );
 
 export default App;
--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -2,9 +2,21 @@ import React, { Component } from "react";
 
 class PostForm extends Component {
   render() {
-    return <div>
-      <h1>Add Post</h1>
-    </div>;
+    return (
+      <div>
+        <h1>Add Post</h1>
+        <form>
+          <div className="form-group">
+            <label>Title</label>
+            <input type="text" className="form-control" />
+          </div>
+          <div className="form-group">
+            <label>Body</label>
+            <textarea type="text" className="form-control" />
+          </div>
+        </form>
+      </div>
+    );
   }
 }
 
