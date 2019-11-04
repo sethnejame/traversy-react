65d95d51c50a5e822bcc2b2d78a35abcb3162696
Seth NeJame <sethnejame@gmail.com>
2019-11-04 06:54:26 +0100

    Adding jsonplaceholder fetch call @ Posts component

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,15 +1,19 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class Posts extends Component {
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then(response => response.json())
+      .then(data => console.log(data));
+  }
+  
   render() {
     return (
       <div>
         <h2>Posts</h2>
       </div>
-    )
+    );
   }
 }
-
-
 
 export default Posts;
