d29940d26bb8e838689eb4f53c70b4251ced5dc1
Seth NeJame <sethnejame@gmail.com>
2019-11-04 06:56:04 +0100

    Adding state to Posts

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
 
 class Posts extends Component {
+  state = {
+    posts: []
+  }
+
   componentDidMount() {
     fetch("https://jsonplaceholder.typicode.com/posts")
       .then(response => response.json())
       .then(data => console.log(data));
   }
-  
+
   render() {
     return (
       <div>
