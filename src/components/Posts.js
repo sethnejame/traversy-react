e2280ec0a1035356354e21bf7c28b4ff5f7d6cdb
Seth NeJame <sethnejame@gmail.com>
2019-11-04 07:03:22 +0100

    Mapping fetched data to Posts state, iterating over state to display @ Posts body

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -3,18 +3,26 @@ import React, { Component } from "react";
 class Posts extends Component {
   state = {
     posts: []
-  }
+  };
 
   componentDidMount() {
     fetch("https://jsonplaceholder.typicode.com/posts")
       .then(response => response.json())
-      .then(data => console.log(data));
+      .then(data => this.setState({ posts: data }));
   }
 
   render() {
+    const postList = this.state.posts.map(post => (
+      <div key={post.id}>
+        <h4>{post.title}</h4>
+        <p>{post.body}</p>
+        <hr />
+      </div>
+    ));
     return (
       <div>
-        <h2>Posts</h2>
+        <h1>Posts</h1>
+        {postList}
       </div>
     );
   }
