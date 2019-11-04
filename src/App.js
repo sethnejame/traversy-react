259c390a2040b36991e0f3b9caed3fafe06f1ff2
Seth NeJame <sethnejame@gmail.com>
2019-11-04 07:06:27 +0100

    Adding PostForm, wiring up to App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Posts from "./components/Posts";
+import PostForm from "./components/PostForm";
 
 const App = () => (
   <>
     <Posts />
+    <PostForm />
   </>
 );
 
---
+++ src/components/PostForm.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class PostForm extends Component {
+  render() {
+    return <div>
+      <h1>Add Post</h1>
+    </div>;
+  }
+}
+
+export default PostForm;
