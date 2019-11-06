2a4040c4d938921a99b037ca9a6ce4ccf29735bb
Seth NeJame <sethnejame@gmail.com>
2019-11-06 06:46:57 +0100

    Wrapping App component in Provider, passing in store (need to create store)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
 import Posts from "./components/Posts";
 import PostForm from "./components/PostForm";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
 
 const App = () => (
-  <div className="container">
-    <PostForm />
-    <hr />
-    <Posts />
-  </div>
+  <Provider store={store}>
+    <div className="container">
+      <PostForm />
+      <hr />
+      <Posts />
+    </div>
+  </Provider>
 );
 
 export default App;
